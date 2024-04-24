9b8421807e9a9cebf2f1d23d1a7e8d4264bd5894
ZRGX <139933536+RGX650@users.noreply.github.com>
2024-04-24 20:01:54 +0200

    Update install.py

--- install.py
+++ install.py
@@ -5,13 +5,15 @@ import launch
 # if not launch.is_installed("aitextgen"):
 #     launch.run_pip("install aitextgen==0.6.0", "requirements for MagicPrompt")
 
-if not launch.is_installed("transformers==4.36.2"):
-    if not launch.is_installed("accelerate==0.25.0"):
-        launch.run_pip("install accelerate==0.25.0", "requirements for Image Caption")
-    launch.run_pip("install transformers==4.36.2", "requirements for Image Caption")
+if not launch.is_installed("transformers>=4.36.2"):
+    if not launch.is_installed("accelerate>=0.25.0"):
+        launch.run_pip("install accelerate==0.28.0", "requirements for Image Caption")
+    launch.run_pip("install transformers==4.38.2", "requirements for Image Caption")
     
 if not launch.is_installed("open_clip_torch"):
     launch.run_pip("install open_clip_torch", "requirements for Image Caption")
     
 if not launch.is_installed("mediafire-dl"):
     launch.run_pip("install git+https://github.com/Juvenal-Yescas/mediafire-dl", "requirements for Image Caption")
+
+
